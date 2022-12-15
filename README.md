4bbb80475c3d6a6755c260c0811a5d5d3b66770c
tuthoeng123 <70209314+tuthoeng123@users.noreply.github.com>
2022-12-15 07:42:07 +0700

    asd

---
+++ README.md
@@ -0,0 +1,7 @@
+# forkify Project
+
+Small MPV recipe application with custom recipe uploads.
+
+Enter an ingredient (like 'pizza') in a search bar to fetch the recipes.
+
+Written on a pure js, build with parcel. SCSS pre-processor. 
---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// Global all-across variables in one place
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 40;
+export const KEY = '225e5e88-3bfd-47cb-891a-1e0a9141cc5f';
+export const TEST_RECIPE_ID = '5ed6604591c37cdc054bc886';
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
